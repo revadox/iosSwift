c7008e3b2617e9c01af0e05eb9de16453c9c8f21
vd <vdbazinga79@gmail.com>
2017-12-25 16:34:20 +0530

    add 6 card

--- iosSwift/ViewController.swift
+++ iosSwift/ViewController.swift
@@ -21,18 +21,29 @@ class ViewController: UIViewController {
     }
 
     @IBOutlet weak var flipCountNumber: UILabel!
+    
+    @IBOutlet var cardButton: [UIButton]!
+    
+    var emojiChoices = ["👻","🐶","👻","🐶","👻","🐶"]
+    
     @IBAction func touchFirtButton(_ sender: UIButton) {
         flipCount += 1
-        flipCard(withEmoji : "👻",on : sender)
+        if  let cardNumber = cardButton.index(of: sender)  {
+            print("cardnumber, \(cardNumber)")
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }
+        else {
+            
+            print("something wrong")
+            
+        }
+        
     }
-    @IBAction func touchSecondButton(_ sender: UIButton) {
-        flipCount += 1
-        flipCard(withEmoji : "🐶",on : sender)
-    }
+
     func flipCard(withEmoji emoji:String, on button: UIButton){
             if button.currentTitle == emoji {
                 button.setTitle("", for:.normal)
-                button.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.5174844981, blue: 0.09545681811, alpha: 1)
+                button.backgroundColor = #colorLiteral(red: 0.9244548678, green: 0.4297498167, blue: 0.09075621516, alpha: 1)
         }
             else {
                 button.setTitle(emoji, for: .normal)
@@ -40,24 +51,7 @@ class ViewController: UIViewController {
         }
         
     
-            
-            /*
-             [UIView transitionWithView:flipContainerView
-             duration:1
-             options:UIViewAnimationOptionTransitionFlipFromLeft
-             animations:^{
-             
-             if (!flipped) {
-             [frontCard setHidden:YES];
-             [flipContainerView addSubview:backCard.view];
-             flipped = YES;
-             } else {
-             [frontCard setHidden:NO];
-             [backCard removeFromSuperview]; //or hide it.
-             }
-             
-             } completion:nil];
-             */
+        
             
         }
         
