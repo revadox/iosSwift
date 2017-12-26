a72bc558168b572a5e6955a664ccd83851591de7
vd <vdbazinga79@gmail.com>
2017-12-26 09:34:31 +0530

    mvc add

---
+++ iosSwift/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  iosSwift
+//
+//  Created by darshan on 25/12/17.
+//  Copyright © 2017 darshan. All rights reserved.
+//
+
+import Foundation
+
+
+struct Card
+{
+     
+    
+}
---
+++ iosSwift/MainModel.swift
@@ -0,0 +1,18 @@
+//
+//  MainModel.swift
+//  iosSwift
+//
+//  Created by darshan on 25/12/17.
+//  Copyright © 2017 darshan. All rights reserved.
+//
+
+import Foundation
+
+class MainModel{
+    var cards: Array<Card>
+    
+    func chooseCard(at index: Int) {
+         
+    }
+    
+}
